85c8fac27a704e464a0349b4935ed2917c1fec22
Shubhamkr2610 <shubham1bcabnc18@gmail.com>
2022-03-22 11:23:26 +0530

    feat: created  landing page , converted html code to react and added routes

---
+++ shoeshopper-react/src/pages/index.js
@@ -0,0 +1,6 @@
+export  { Cart } from "./cart-page/Cart";
+export { Landingpage } from "./landing-page/Landingpage";
+export {Login} from "./login/Login";
+export {Products} from "./product-page/Products";
+export {Signup} from "./signup/Signup";
+export { Wishlist } from "./wishlist-page/Wishlist";
